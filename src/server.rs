cc1689149203e14531def42151660c30c4704107
yoyobara <yotambarak6@gmail.com>
2023-12-11 23:19:42 +0200

    general W

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,20 @@
+use std::io::Write;
+
+// struct server struct with generic output method
+struct Server<W: Write> {
+    output: W
+}
+
+impl<W: Write> Server<W> {
+
+    /*
+     * create a new Server with any output stream
+     */
+    pub fn new(output_stream: W) -> Server<W> {
+        Server { output: output_stream }
+    }
+}
 
 fn main() {
-    println!("this is server");
+    let mut server = Server::new(std::fs::File::create("wow.txt",).unwrap());
 }
